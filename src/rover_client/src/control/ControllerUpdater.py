777d55e7f50e35a2e48295afef79aed0f79591a8
Will Richards <jwilt9000@gmail.com>
2020-11-07 10:16:14 -0800

    added different controller profiles.

--- src/rover_client/src/control/ControllerUpdater.py
+++ src/rover_client/src/control/ControllerUpdater.py
@@ -6,6 +6,8 @@ import threading
 
 from rover_main.msg import controllerMap
 
+CONTROLLER_TYPE = ""
+
 # Local dictionary of the state of the controller
 CONTROLLER_DICT = {
 
@@ -36,6 +38,10 @@ def start_controller_monitor():
     """Start the asynced thread to register controller inputs"""
     
     rospy.loginfo("Staring Controller Thread...")
+
+    # The launch file contains a parameter to set the correct controller type
+    global CONTROLLER_TYPE
+    CONTROLLER_TYPE = rospy.get_param("controller_type").strip()
        
     # Create and start the asynico thread
     thread = threading.Thread(target=get_controller_states, args=())
@@ -57,72 +63,75 @@ def get_controller_states():
     # Refernce the global CONTROLLER_DICT to be used locally
     global CONTROLLER_DICT
 
-    # Events are called when the button is pushed and when it is released
-    while not rospy.is_shutdown():
-
-        # X / A
-        CONTROLLER_DICT["Key_X"] = bool(controller.get_button(0))
-
-        # Circle / B
-        CONTROLLER_DICT["Key_Circle"] = bool(controller.get_button(1))
-
-        # Triangle / X
-        CONTROLLER_DICT["Key_Triangle"] = bool(controller.get_button(3))
-
-        # Square / Y
-        CONTROLLER_DICT["Key_Square"] = bool(controller.get_button(2))
+    # Use the XBOX controller layout
+    if CONTROLLER_TYPE == "xbox":
         
-        # Left Bumper
-        CONTROLLER_DICT["Key_Bumper_Left"] = bool(controller.get_button(4))
+        # Events are called when the button is pushed and when it is released
+        while not rospy.is_shutdown():
 
-        # Right Bumper
-        CONTROLLER_DICT["Key_Bumper_Right"] = bool(controller.get_button(5))
+            # X / A
+            CONTROLLER_DICT["Key_X"] = bool(controller.get_button(0))
 
-        # Dpad-Parser (Left / Right)
-        if controller.get_hat(0)[0] > 0.2:
-            CONTROLLER_DICT["Key_Dpad_Right"] = True
-            CONTROLLER_DICT["Key_Dpad_Left"] = False
-        elif controller.get_hat(0)[0] < -0.2:
-            CONTROLLER_DICT["Key_Dpad_Right"] = False
-            CONTROLLER_DICT["Key_Dpad_Left"] = True
-        else:
-            CONTROLLER_DICT["Key_Dpad_Right"] = False
-            CONTROLLER_DICT["Key_Dpad_Left"] = False
+            # Circle / B
+            CONTROLLER_DICT["Key_Circle"] = bool(controller.get_button(1))
 
-        # Dpad-Parser (Up / Down)
-        if controller.get_hat(0)[1] > 0.2:
-            CONTROLLER_DICT["Key_Dpad_Up"] = True
-            CONTROLLER_DICT["Key_Dpad_Down"] = False
-        elif controller.get_hat(0)[1] < -0.2:
-            CONTROLLER_DICT["Key_Dpad_Up"] = False
-            CONTROLLER_DICT["Key_Dpad_Down"] = True
-        else:
-            CONTROLLER_DICT["Key_Dpad_Up"] = False
-            CONTROLLER_DICT["Key_Dpad_Down"] = False
+            # Triangle / X
+            CONTROLLER_DICT["Key_Triangle"] = bool(controller.get_button(3))
+
+            # Square / Y
+            CONTROLLER_DICT["Key_Square"] = bool(controller.get_button(2))
+            
+            # Left Bumper
+            CONTROLLER_DICT["Key_Bumper_Left"] = bool(controller.get_button(4))
+
+            # Right Bumper
+            CONTROLLER_DICT["Key_Bumper_Right"] = bool(controller.get_button(5))
+
+            # Dpad-Parser (Left / Right)
+            if controller.get_hat(0)[0] > 0.2:
+                CONTROLLER_DICT["Key_Dpad_Right"] = True
+                CONTROLLER_DICT["Key_Dpad_Left"] = False
+            elif controller.get_hat(0)[0] < -0.2:
+                CONTROLLER_DICT["Key_Dpad_Right"] = False
+                CONTROLLER_DICT["Key_Dpad_Left"] = True
+            else:
+                CONTROLLER_DICT["Key_Dpad_Right"] = False
+                CONTROLLER_DICT["Key_Dpad_Left"] = False
+
+            # Dpad-Parser (Up / Down)
+            if controller.get_hat(0)[1] > 0.2:
+                CONTROLLER_DICT["Key_Dpad_Up"] = True
+                CONTROLLER_DICT["Key_Dpad_Down"] = False
+            elif controller.get_hat(0)[1] < -0.2:
+                CONTROLLER_DICT["Key_Dpad_Up"] = False
+                CONTROLLER_DICT["Key_Dpad_Down"] = True
+            else:
+                CONTROLLER_DICT["Key_Dpad_Up"] = False
+                CONTROLLER_DICT["Key_Dpad_Down"] = False
 
 
 
-        # Left Joystick X
-        CONTROLLER_DICT["Joystick_LeftX"] = controller.get_axis(0)
-        
+            # Left Joystick X
+            CONTROLLER_DICT["Joystick_LeftX"] = controller.get_axis(0)
+            
 
-        # Left Joystick Y
-        CONTROLLER_DICT["Joystick_LeftY"] = controller.get_axis(1)
+            # Left Joystick Y
+            CONTROLLER_DICT["Joystick_LeftY"] = controller.get_axis(1)
 
-        # Right Joystick X
-        CONTROLLER_DICT["Joystick_RightX"] = controller.get_axis(3)
+            # Right Joystick X
+            CONTROLLER_DICT["Joystick_RightX"] = controller.get_axis(3)
 
-        # Right Joystick Y
-        CONTROLLER_DICT["Joystick_RightY"] = controller.get_axis(4)
+            # Right Joystick Y
+            CONTROLLER_DICT["Joystick_RightY"] = controller.get_axis(4)
 
-        # Right Trigger
-        CONTROLLER_DICT["Trigger_Right"] = convert_trigger(controller.get_axis(5))
+            # Right Trigger
+            CONTROLLER_DICT["Trigger_Right"] = convert_trigger(controller.get_axis(5))
 
-        # Left Trigger
-        CONTROLLER_DICT["Trigger_Left"] = convert_trigger(controller.get_axis(2))
+            # Left Trigger
+            CONTROLLER_DICT["Trigger_Left"] = convert_trigger(controller.get_axis(2))
 
-        # Register new events
-        pygame.event.pump()
+            # Register new events
+            pygame.event.pump()
             
 
 def getCurrentControllerState():
