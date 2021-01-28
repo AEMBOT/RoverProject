71a36a78a822c2ab1914c785c2f845a895ba19b0
Will <jwilt9000@gmail.com>
2021-01-28 10:01:15 -0800

    Added basic servo functionality to arudino side

--- arduino_sketches/I2C_MCP_Motor_Controller/I2C_MCP_Motor_Controller.ino
+++ arduino_sketches/I2C_MCP_Motor_Controller/I2C_MCP_Motor_Controller.ino
@@ -2,8 +2,19 @@
 
 //Includes required to use Roboclaw library
 #include <SoftwareSerial.h>
+#include <Servo.h>
 #include <Wire.h>
+
 #include "RoboClaw.h"
+
+// All of the motors controlling the 
+Servo front_left_servo;
+Servo front_right_servo;
+Servo back_left_servo;
+Servo back_right_servo;
+
+// Array to hold all the servos and make them easier to use
+Servo servo_array[4];
 
 // Set I2C Device address
 #define SLAVE_ADDRESS 0x08
@@ -17,18 +28,45 @@ int motor_speeds[] = {64, 64, 64, 64, 64, 64};
 // Values from the last loop to not send tons of information to the controller, only when new info is needed
 int last_motor_speeds[] = {64, 64, 64, 64, 64, 64};
 
-//See limitations of Arduino SoftwareSerial
+// Array to hold the angles of each of the servos
+int wheel_angles[] = {0, 0, 0, 0};
+
+// Array to hold the previous angles of each of the servos
+int last_wheel_angles[] = {0, 0, 0, 0};
+
+//Set the serial to run on the standard TX and RX pins
 SoftwareSerial serial(0,1);	
 RoboClaw roboclaw(&serial,10000);
 
+// Address of the first motor controller
 #define controller_1_addr 0x80
 
 void setup() {
 
-   Wire.begin(SLAVE_ADDRESS);
-   Wire.onReceive(receiveData);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+
   //Open roboclaw serial ports
   roboclaw.begin(115200);
+
+  // Run the front left side servo on pin 9
+  front_left_servo.attach(9);
+
+  // Run the front right side servo on pin 10
+  front_right_servo.attach(10)
+
+  // Run the back left side servo on pin 5
+  back_left_servo.attach(5);
+
+  // Run the back right side servo on pin 6
+  back_right_servo.attach(6);
+
+  // Add all servos to spots in the servo array
+  servo_array[0] = front_left_servo;
+  servo_array[1] = front_right_servo;
+  servo_array[2] = back_left_servo;
+  servo_array[3] = back_right_servo;
+
 }
 
 void receiveData(int bytecount)
@@ -44,6 +82,13 @@ void receiveData(int bytecount)
       motor_speeds[i] = int(Wire.read());
     }
   }
+
+  // Get information intended for the servos and store them in the servo array
+  else if (register_id == 1){
+    for (int i = 0; i < bytecount; i++){
+      wheel_angles[i] = angle_to_servo(int(Wire.read()));
+    }
+  }
   
 }
 /**
@@ -55,10 +100,16 @@ void receiveData(int bytecount)
    * 3 - Controller 2 : Motor 2
    * 4 - Controller 3 : Motor 1
    * 5 - Controller 3 : Motor 2
+ * 1 - Servo Turning Control
+   * 0 - Front Left Servo
+   * 1 - Front Right Servo
+   * 2 - Back Left Servo
+   * 3 - Back Right Servo
  */
 
 void loop() {
     
+    // Using sizeof the array divided by the sizeof an element in the array to get the length of the array of motors
     for(int i=0; i<(sizeof(motor_speeds) / sizeof(motor_speeds[0])); i++){
        if (motor_speeds[i] != last_motor_speeds[i]){
 
@@ -77,5 +128,25 @@ void loop() {
           last_motor_speeds[i] = motor_speeds[i];
        }
     }
+
+    // Get all values in the wheel_angles array
+    for (int i=0; i<(sizeof(wheel_angles) / sizeof(wheel_angles[0])); i++){
+
+      // Check if the new angle is the same as last to avoid sending useless information
+      if (wheel_angles[i] != last_wheel_angles[i]){
+        // Write the angle to the corresponding servo 
+        servo_array[i].write(wheel_angles[i])
+
+        // Set the last to the current
+        last_wheel_angles[i] = wheel_angles[i];
+      }
+    }
     
+}
+
+// Converts normal angles into matching angles of the geared down 
+int angle_to_servo(int input){
+
+  // Implement at a later date
+  return input
 }
