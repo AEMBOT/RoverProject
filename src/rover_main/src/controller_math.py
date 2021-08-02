e230344a1827615dfb99bee3941c3ee4a5aa274e
Will <jwilt9000@gmail.com>
2021-08-02 09:22:27 -0700

    Implemented steering differential calculations. And added speed scalars to ensure the wheel speeds are normalized to less than 1.

--- src/rover_main/src/controller_math.py
+++ src/rover_main/src/controller_math.py
@@ -11,11 +11,16 @@ AXLE_DISTANCE = 1
 # Current radii of the circle to calculate angles from
 circleRadius = 0
 
+# Whether or not to reorder values to turn the other direction
+turnRight = False
 
-def updateCircleRadius(turnRadius):
+
+def updateCircleRadius(turnRadius, controllerInputs):
+    global circleRadius, turnRight
     """Updates the inner circles radius"""
     b = 0.52345 / turnRadius
     circleRadius = ((0.5 * AXLE_DISTANCE) / math.sin(b))
+    turnRight = controllerInputs > 0
 
 def _getInnerCircleY(x):
     """Get the Y axis value at a given X of the inner circle"""
@@ -45,16 +50,111 @@ def _convertSlopeToAngle(x):
     """Convert a given slope into an angle"""
     return math.degrees(abs(math.atan(x)))
 
-def getWheelAngle(wheel):
-    """Returns the required angle of the wheels enum"""
-    if wheel == 0:
-        return _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2))) + 90
-    elif wheel == 1:
-        return _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2))) + 90
-    elif wheel == 2:
-        return 90 - _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2)))
-    elif wheel == 3:
-        return 90 - _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2)))
+def remapBounds(low1, low2, high1, high2, value):
+    """Remap the given value into a new range"""
+    return (low2 + (value - low1) * (high2 - low2) / (high1 - low1))
 
-    
-        
+def _calcSpeedIncrease(value):
+    """Calculate the speed increase for the given wheel"""
+    innerCircumfrence = 2 * math.pi * circleRadius
+    outerCircumfrence = 2 * math.pi * (circleRadius+TRACK_WIDTH)
+
+    return (innerCircumfrence / outerCircumfrence) * value
+
+def map_to_motor_controller(value):
+    """Convert from -1 to 1 to 0 to 127 with 64 in the middle to accommodate the motor controller values"""
+    if value >= 0:
+        if 64 + abs(round(value * 64)) > 127:
+            return 127
+        return (64 + abs(round(value * 64)))
+    else:
+        return (64 - abs(round(value * 64)))
+
+def getNormalizedWheelSpeed(value, angle_input, wheel):
+    """Prevent the wheel speeds from exceeding 1 to allow for it to still turn while at full power"""
+
+    speedDivisor = 1
+    outerSpeed = value + _calcSpeedIncrease(value)
+    if outerSpeed > 1:
+        speedDivisor = outerSpeed
+
+    # If we are trying to turn
+    if angle_input != 0:
+        if not turnRight:
+            # Front Left
+            if wheel == 0:
+                return map_to_motor_controller(value / speedDivisor)
+
+            # Front Right
+            elif wheel == 1:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+            
+            # Back Left
+            elif wheel == 2:
+                return map_to_motor_controller(value / speedDivisor)
+
+            # Back Right
+            elif wheel == 3:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+
+            # Middle Right
+            elif wheel == 4:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+            # Middle Left
+            elif wheel == 5:
+                return map_to_motor_controller(value / speedDivisor)
+                
+        else:
+            # Front Left
+            if wheel == 0:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+
+            # Front Right
+            elif wheel == 1:
+                return map_to_motor_controller(value / speedDivisor)
+            
+            # Back Left
+            elif wheel == 2:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+
+            # Back Right
+            elif wheel == 3:
+                return map_to_motor_controller(value / speedDivisor)
+
+            # Middle Right
+            elif wheel == 4:
+                return map_to_motor_controller(value / speedDivisor)
+            # Middle Left
+            elif wheel == 5:
+                return map_to_motor_controller(outerSpeed / speedDivisor)
+    else:
+        return map_to_motor_controller(value)
+
+def getWheelAngle(wheel, angleInput):
+    """Returns the required angle of the wheels enum"""
+    # Turning Left
+    if angleInput != 0:
+        if not turnRight:
+            if wheel == 0:
+                return _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2))) + 90
+            elif wheel == 1:
+                return _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2))) + 90
+            elif wheel == 2:
+                return 90 - _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2)))
+            elif wheel == 3:
+                return 90 - _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2)))
+
+        # Turning right
+        else:
+            if wheel == 0:
+                return 90 - _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2)))
+            elif wheel == 1:
+                return  90 - _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2)))
+            elif wheel == 2:
+                return _convertSlopeToAngle(_getInnerCircleDerivative(_getInnerCircleX(AXLE_DISTANCE / 2))) + 90 
+            elif wheel == 3:
+                return _convertSlopeToAngle(_getOuterCircleDerivative(_getOuterCircleX(AXLE_DISTANCE / 2))) + 90
+
+    # If we aren't trying to turn set all wheels to face forward
+    else:
+        return 0
